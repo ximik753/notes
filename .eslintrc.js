56dff0cc83a711c455fe9be1e6a932cba28d4b8e
ximik <sankovdiman@mail.ru>
2021-04-27 08:21:28 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'object-curly-spacing': ['error', 'never'],
+    quotes: ['error', 'single'],
+    'space-before-function-paren': ['error', 'never'],
+    'no-return-assign': 'off'
+  }
+}
