f49dfe093d7608ebc84162028d3ae1dd83f8b841
Ximik <sankovdiman@mail.ru>
2021-05-04 17:30:45 +0300

    добавление спиннера

---
+++ src/components/UI/AppSpinner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    :class="[
+      'spinner-border',
+      {
+        'spinner-border-sm': small,
+        [`text-${color}`]: color
+      }
+    ]"
+    role="status"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'AppSpinner',
+  props: {
+    small: {
+      type: Boolean,
+      default: false
+    },
+    color: {
+      type: String,
+      validator(value) {
+        return ['primary'].includes(value)
+      }
+    }
+  }
+}
+</script>
