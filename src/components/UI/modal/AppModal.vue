5e8ba98ba387a293f464b226b1d06e6842ef173b
Ximik <sankovdiman@mail.ru>
2021-05-15 11:31:41 +0300

    Добавление модального окна и фикс некоторых стилей

---
+++ src/components/UI/modal/AppModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="modal">
+    <div class="modal__backdrop">
+      <div class="modal__window">
+        <div class="modal__header">
+          <h6>
+            <slot name="title"/>
+          </h6>
+          <div class="modal__close" @click="$emit('close')">&times;</div>
+        </div>
+        <div class="modal__body">
+          <slot name="body"/>
+        </div>
+        <div class="modal__footer">
+          <slot name="footer"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppModal',
+  emits: ['close']
+}
+</script>
+
+<style scoped lang="scss">
+@import "modal";
+
+h6 {
+  margin: 0;
+}
+</style>
