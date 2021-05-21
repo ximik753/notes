6bcdfa3fc26782a9ebf0d1643ee09c39afed9bc7
Ximik <sankovdiman@mail.ru>
2021-05-21 17:20:16 +0300

    Вынесение компонента с анимацией модального окна в отдельный компонент

---
+++ src/components/UI/modal/AppModalAnimation.vue
@@ -0,0 +1,18 @@
+<template>
+  <teleport :to="to">
+    <transition name="fade">
+      <slot/>
+    </transition>
+  </teleport>
+</template>
+
+<script>
+export default {
+  name: 'AppModalAnimation',
+  props: {
+    to: {
+      default: 'body'
+    }
+  }
+}
+</script>
--- src/components/notes/notesContainer/notesList/note/AppRemoveNoteModal.vue
+++ src/components/notes/notesContainer/notesList/note/AppRemoveNoteModal.vue
@@ -1,34 +1,33 @@
 <template>
-  <teleport to="body">
-    <transition name="fade">
-      <app-modal
-        v-if="show"
-        @close="close"
-      >
-        <template #title>Удаление</template>
-        <template #body>
-          Вы действительно хотите удалить заметку с названим {{title}}
-          <br>
-          <br>
-          Отменить это действие <strong>будет не возможно</strong>
-        </template>
-        <template #footer>
-          <div class="d-flex justify-content-end">
-            <button class="btn btn-primary mr-3" @click="close" :disabled="$store.state.notes.removing">Отмена</button>
-            <button class="btn btn-danger" :disabled="$store.state.notes.removing" @click="$emit('remove')">Удалить</button>
-          </div>
-        </template>
-      </app-modal>
-    </transition>
-  </teleport>
+  <app-modal-animation>
+    <app-modal
+      v-if="show"
+      @close="close"
+    >
+      <template #title>Удаление</template>
+      <template #body>
+        Вы действительно хотите удалить заметку с названим {{title}}
+        <br>
+        <br>
+        Отменить это действие <strong>будет не возможно</strong>
+      </template>
+      <template #footer>
+        <div class="d-flex justify-content-end">
+          <button class="btn btn-primary mr-3" @click="close" :disabled="$store.state.notes.removing">Отмена</button>
+          <button class="btn btn-danger" :disabled="$store.state.notes.removing" @click="$emit('remove')">Удалить</button>
+        </div>
+      </template>
+    </app-modal>
+  </app-modal-animation>
 </template>
 
 <script>
 import AppModal from '../../../../UI/modal/AppModal'
+import AppModalAnimation from '../../../../UI/modal/AppModalAnimation'
 
 export default {
   name: 'AppRemoveNoteModal',
-  components: {AppModal},
+  components: {AppModalAnimation, AppModal},
   props: ['title', 'show'],
   emits: ['close', 'remove'],
   methods: {
